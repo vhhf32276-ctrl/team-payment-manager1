28a6bc54834e3e6333d528f590a5fcd2169ea78c
vhhf32276-ctrl <vhhf32276@gmail.com>
2026-09-06 16:15:34 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+import os
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "change-this-secret-key")
+    DATABASE = os.path.join(BASE_DIR, "instance", "payments.sqlite3")
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
